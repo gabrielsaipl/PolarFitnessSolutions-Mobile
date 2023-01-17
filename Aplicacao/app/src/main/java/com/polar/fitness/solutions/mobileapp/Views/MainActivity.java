c3f5620938ec3a35f41428ce26707bfd747b1e97
Gabriel <2211901@myipleiria.pt>
2023-01-17 05:34:50 +0000

    organizacao de ficheiros

--- Aplicacao/app/src/main/java/com/polar/fitness/solutions/mobileapp/Views/MainActivity.java
+++ Aplicacao/app/src/main/java/com/polar/fitness/solutions/mobileapp/Views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.polar.fitness.solutions.mobileapp;
+package com.polar.fitness.solutions.mobileapp.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.polar.fitness.solutions.mobileapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
