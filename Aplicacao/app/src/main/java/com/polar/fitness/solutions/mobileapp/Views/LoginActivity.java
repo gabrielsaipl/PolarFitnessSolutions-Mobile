e5de03b827df6cbeb39851f6510bc5250a68c2bf
Gabriel <2211901@myipleiria.pt>
2023-01-17 15:30:19 +0000

    vista de login e lateracao de pastas

---
+++ Aplicacao/app/src/main/java/com/polar/fitness/solutions/mobileapp/Views/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.polar.fitness.solutions.mobileapp.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.polar.fitness.solutions.mobileapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
