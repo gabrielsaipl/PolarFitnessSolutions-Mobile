b801d913882458a9ceae583a33117f18b8ab5bf4
Gabriel <2211901@my.ipleiria.pt>
2023-01-21 19:06:25 +0000

    rollback

--- Aplicacao/app/src/main/java/com/polar/fitness/solutions/mobileapp/Models/SingletonGestorUsers.java
+++ Aplicacao/app/src/main/java/com/polar/fitness/solutions/mobileapp/Models/SingletonGestorUsers.java
@@ -1,5 +1,6 @@
 package com.polar.fitness.solutions.mobileapp.Models;
 
+import android.app.DownloadManager;
 import android.content.Context;
 import android.widget.Toast;
 
@@ -9,12 +10,16 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.polar.fitness.solutions.mobileapp.Listeners.LoginListener;
+import com.polar.fitness.solutions.mobileapp.Listeners.nutrition_plansListener;
 import com.polar.fitness.solutions.mobileapp.Listeners.RegisterListener;
 import com.polar.fitness.solutions.mobileapp.R;
 import com.polar.fitness.solutions.mobileapp.Utils.UserJsonParser;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,11 +31,13 @@ public class SingletonGestorUsers {
     private static RequestQueue volleyQueue = null;
     private UserDBHelper usersDB = null;
     private ArrayList<User> users;
-    private final static String mUrlLogin = "http://192.168.1.14/github/polarfitnesssolutions-portal/polarfitnesssolutions/backend/web/api/user/login";
+    private ArrayList<Nutrition_plan> nutrition_plans;
 
-    private final static String mUrlSignup = "http://192.168.1.14/github/polarfitnesssolutions-portal/polarfitnesssolutions/backend/web/api/user/signup";
+    private final static String mUrlLogin = "http://10.0.2.2/github/polarfitnesssolutions-portal/polarfitnesssolutions/backend/web/api/user/login";
+    private final static String mUrlnutrition_plan = "http://10.0.2.2/github/polarfitnesssolutions-portal/polarfitnesssolutions/backend/web/api/nutrition_plans{/client_id=2}";
+    private final static String mUrlSignup = "http://10.0.2.2/github/polarfitnesssolutions-portal/polarfitnesssolutions/backend/web/api/user/signup";
     private LoginListener loginListener;
-
+    private nutrition_plansListener nutrition_plansListener;
     private RegisterListener registerListener;
     
     public static synchronized SingletonGestorUsers getInstance(Context contexto){
@@ -45,6 +52,38 @@ public class SingletonGestorUsers {
         users = new ArrayList<>();
         usersDB = new UserDBHelper(contexto);
     }
+
+    public ArrayList<Nutrition_plan> getNutrition_plansBD() {
+        return nutrition_plans = usersDB.getAllNutrition_planBD();
+    }
+
+    //Buscar os livros do ficheiro criado para a lista
+    public void setLivros(ArrayList<Nutrition_plan> list) {
+        this.nutrition_plans = list;
+    }
+    public Nutrition_plan getNutrition_plan(long id) {
+        for (Nutrition_plan nutrition_plan : nutrition_plans) {
+            return nutrition_plan;
+        }
+        return null;
+    }
+
+    public void addNutrition_plansBD(ArrayList<Nutrition_plan> list) {
+        usersDB.removeAllNutrition_planBD();
+        for (Nutrition_plan nutrition_plan : list) {
+            addNutrition_planBD(nutrition_plan);
+        }
+    }
+
+    public void addNutrition_planBD(Nutrition_plan nutrition_plan)
+    {
+        usersDB.addNutrition_planBD(nutrition_plan);
+    }
+    public void setNutrition_plansListener(nutrition_plansListener nutrition_plansListener)
+    {
+        this.nutrition_plansListener = nutrition_plansListener;
+    }
+
 
     public void loginAPI(final String username,final String password, Context contexto){
 
@@ -116,6 +155,36 @@ public class SingletonGestorUsers {
         };
         volleyQueue.add(reqRegister);
 }
+
+    //Método para ir buscar todos os planos de nutrição a API
+    public void getAllNutrition_plansAPI(final Context contexto)
+    {
+        if(!UserJsonParser.isConnectionInternet(contexto))
+        {
+            Toast.makeText(contexto, R.string.no_internet, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET, mUrlnutrition_plan, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                nutrition_plans = UserJsonParser.parserJsonNutrition_plan(response);
+                addNutrition_plansBD(nutrition_plans);
+                if(nutrition_plansListener!=null)
+                {
+
+                    nutrition_plansListener.onRefreshListNutrition_plans(nutrition_plans);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(contexto, error.getMessage(), Toast.LENGTH_SHORT).show();
+                return;
+            }
+        });
+        volleyQueue.add(req);
+    }
 
     public void setLoginListener(LoginListener loginListener) {
         this.loginListener = loginListener;
